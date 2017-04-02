d9372b76deafad17a8f80863c35681d15f570961
Santiago Alexis Quiroz Bazan <saquirozbazan@gmail.com>
2017-04-02 11:01:35 -0500

    creating observable

---
+++ src/main/java/com/santiagoone/training/rxjavaexamples/Main.java
@@ -0,0 +1,18 @@
+package com.santiagoone.training.rxjavaexamples;
+
+/**
+ * Created by Ricardo Alexis Quiroz Bazan 01/04/17
+ *
+ *  Practices in RxJava 2
+ */
+public class Main {
+    public static void main(String[] args) {
+        CreatingObservable.theMostSimpleAndVerboseWay();
+        CreatingObservable.theMostSimpleWay();
+        CreatingObservable.thatEmitsASingleValue();
+        CreatingObservable.thatEmitsASingleValueWithSingleClass();
+        CreatingObservable.fromMultiplesObservables();
+        CreatingObservable.fromCollection();
+        CreatingObservable.noNullValues();
+    }
+}
